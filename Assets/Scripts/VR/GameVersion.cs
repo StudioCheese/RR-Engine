9cb1cd87f2b234f47d91ecf022898c52cdb8aaee
The 64th Gamer <KNHanes@student.fullsail.edu>
2022-08-14 12:52:41 -0400

    Pre-1.6.2
    Added AAM Chuck head + cosmeticless building

--- Assets/Scripts/VR/GameVersion.cs
+++ Assets/Scripts/VR/GameVersion.cs
@@ -1,6 +1,6 @@
 public static class GameVersion
 {
     public const string gameName = "Reel to Real";
-    public const string gameVersion = "1.60";
+    public const string gameVersion = "1.62";
     public const string isVR = "false";
 }
